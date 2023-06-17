9f7727af3b2d5a7771101ee728f9c0b8fe5372e7
gr00stic <dmitriy.kharabara@gmail.com>
2023-06-17 13:10:04 +0300

    Edited product and review models

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,12 +1,29 @@
-export class ProductModel {
-	_id: string;
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+@Table
+export class ProductModel extends Model {
+	@Column
 	image: string;
+
+	@Column
 	title: string;
+
+	@Column
 	description: string;
+
+	@Column
 	price: number;
+
+	@Column
 	prevPrice: number;
+
+	@Column
 	rating: number;
-	categories: string[];
+
+	// @Column(DataType.ARRAY(DataType.STRING))
+	// categories: string[];
+
+	@Column(DataType.JSON)
 	characteristics: {
 		[key: string]: string;
 	};
--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,9 +1,25 @@
-export class ReviewModel {
+import { Column, Model, Table } from "sequelize-typescript";
+
+@Table
+export class ReviewModel extends Model {
+	@Column
 	_id: string;
+
+	@Column
 	title: string;
+
+	@Column
 	name: string;
+
+	@Column
 	text: string;
+
+	@Column
 	rating: number;
+
+	@Column
 	createdAt: Date;
+
+	@Column
 	updatedAt: Date;
 }
